b61d7219c67e69c07a4e70c93955ff7d3e70df78
horaciosystem <horacioalexandre.fernandes@gmail.com>
2016-01-23 19:26:06 -0200

    Add request to external api to implement getAll method

--- src/main/java/service/ExchangeRatesHttpService.java
+++ src/main/java/service/ExchangeRatesHttpService.java
@@ -1,25 +1,37 @@
 package service;
 
-import org.json.simple.JSONObject;
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+import model.ResponseError;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class ExchangeRatesHttpService implements ExchangeRateService {
+    private final String ACCESS_KEY = "10dea10860a0cd6fb526efef448f3e99";
+    ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    public JSONObject getAll() {
-        JSONObject foo = new JSONObject();
-        foo.put("hello", "world");
-        return foo;
+    public JsonNode getAll() {
+       return toJson(requestLiveRates());
     }
 
     @Override
-    public JSONObject getByCurrency(String currency) {
-        JSONObject foo = new JSONObject();
-            foo.put("hello", "world");
-        return foo;
+    public JsonNode getByCurrency(String currency) {
+        return null;
     }
 
-    private void foo() {
+    private HttpResponse requestLiveRates() {
+        return HttpRequest.get("http://apilayer.net/api/live")
+                .query("access_key", ACCESS_KEY)
+                .send();
+    }
 
+    private JsonNode toJson(HttpResponse response) {
+        try {
+            return mapper.readValue(response.body(), JsonNode.class);
+        } catch (Exception e) {
+            return new ResponseError(e.getMessage()).get();
+        }
     }
 
 
